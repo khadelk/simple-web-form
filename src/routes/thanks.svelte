bc64e631d108ca7c3b68211084e26df606550ece
Elkeurti <khadidjaelkeurti@gmail.com>
2022-07-29 20:11:49 -0500

    Add html and styling for thank you page

--- src/routes/thanks.svelte
+++ src/routes/thanks.svelte
@@ -1,5 +1,22 @@
 <script>
+	import { goto } from '$app/navigation';
 	import { json } from '$lib/stores';
+	import { slide } from 'svelte/transition';
+	console.log($json);
+
 </script>
 
-<h1>thanks</h1>
+
+<section class="thanks">
+	<div transition:slide class="thanks">
+		<h1>Thanks {$json.name}!</h1>
+		<p>Here's the information you filled out:</p>
+		<ul>
+			{#each Object.keys($json) as key}
+			<li>{key}: {$json[key]}</li>
+			{/each}
+		</ul>
+		<button on:click={() => goto('/')}>Go back to form</button>
+
+	</div>
+</section>
